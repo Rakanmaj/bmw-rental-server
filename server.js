0f7db34a58ae6bcaaeaefa26204567f60e5eb65c
Rakanmaj <22210020@htu.edu.jo>
2026-01-20 14:57:20 +0300

    Add reservations routes and auth middleware

--- server.js
+++ server.js
@@ -1,55 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import pgclient from "./db.js";
 
-// AUTH ROUTES
+import carsRoutes from "./routes/carsRoutes.js";
+import reservationsRoutes from "./routes/reservationsRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
-// CARS ROUTES
-import carsRoutes from "./routes/carsRoutes.js";
+import pgclient from "./db.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // ================= MIDDLEWARES =================
 app.use(cors());
 app.use(express.json());
 
+// ================= ROUTES =================
+// http://localhost:3000/api/cars
+app.use("/api/cars", carsRoutes);
+app.use("/api/reservations", reservationsRoutes);
+app.use("/api/auth", authRoutes);
+
+
 // ================= TEST ROUTES =================
 app.get("/", (req, res) => {
-  res.send("BMW Rental Backend is running ");
+  res.send("BMW Rental API is running");
 });
 
 app.get("/test", (req, res) => {
-  res.json({
-    status: "success",
-    message: "API test successful",
-  });
+  res.json({ message: "API test successful" });
 });
 
-// ================= ROUTES =================
-app.use("/api/auth", authRoutes);
-app.use("/api/cars", carsRoutes);
-
-// ================= 404 HANDLER =================
+// ================= NOT FOUND =================
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
 // ================= START SERVER =================
-pgclient
-  .connect()
-  .then(() => {
-    console.log("Database connected ");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Database connection failed ");
-    console.error(err);
+pgclient.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
   });
+});
