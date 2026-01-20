cabcbe1976093e0e053da621e1a50456854b692f
Rakanmaj <22210020@htu.edu.jo>
2026-01-20 22:43:08 +0300

    Fix Railway env + DB SSL + start server

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pgclient = new pg.Client(process.env.DATABASE_URL);
+const pgclient = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
+});
 
 pgclient.on("error", (err) => {
   console.error("PostgreSQL error:", err);
